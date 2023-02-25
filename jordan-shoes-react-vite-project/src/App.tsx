7386c1420fc7e635efe91b3308f8328dea76482a
Anderson Silva <andssilva.23@gmail.com>
2023-02-25 19:47:17 -0400

    [R] feat: :rocket: criando componente slider

--- jordan-shoes-react-vite-project/src/App.tsx
+++ jordan-shoes-react-vite-project/src/App.tsx
@@ -1,8 +1,12 @@
+import Slider from './components/Slider';
+import GlobalStyle from './styles/global';
+
 function App() {
 
   return (
     <div className="App">
-      
+      <Slider />
+      <GlobalStyle />
     </div>
   )
 }
