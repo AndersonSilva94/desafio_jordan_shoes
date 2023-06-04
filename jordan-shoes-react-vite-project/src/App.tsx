f142ae771c546441e57bc7e3aaf762ba61ecaa50
Anderson Silva <andssilva.23@gmail.com>
2023-06-04 14:48:31 -0400

    [R] feat: :rocket: criando o componente Footer

--- jordan-shoes-react-vite-project/src/App.tsx
+++ jordan-shoes-react-vite-project/src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer';
 import Slider from './components/Slider';
 import Home from './pages/Home';
 import GlobalStyle from './styles/global';
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Slider />
       <Home />
+      <Footer />
       <GlobalStyle />
     </div>
   )
---
+++ jordan-shoes-react-vite-project/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Text } from "./styles";
+
+function Footer() {
+  return (
+    <Container>
+      <Text>
+        Todos os direitos reservados.
+      </Text>
+    </Container>
+  )
+}
+
+export default Footer;
